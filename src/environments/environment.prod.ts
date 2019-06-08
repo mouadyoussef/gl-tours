cbb9f86cf84a805fac297ab9582085051fa83437
Youssef Mouad <imouad.you@gmail.com>
2019-06-08 01:44:42 +0000

    Setting up android deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "http://192.168.1.52:8000/api/",
+  googleMapsAPIKey: "AIzaSyDdbAFyZLnVobr1GTwgAeRQXM5CJORePNQ",
+  apiMapKey: "AIzaSyDdbAFyZLnVobr1GTwgAeRQXM5CJORePNQ"
 };
